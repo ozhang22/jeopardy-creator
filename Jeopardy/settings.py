1191a6f7af88c5ae16899fa0901c76c51b10c704
Oliver Zhang <bestozhang@gmail.com>
2016-07-10 14:53:59 -0400

    Initial configurations of the creator app, and setting up PostgreSQL database

--- Jeopardy/settings.py
+++ Jeopardy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'creator.apps.CreatorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'Jeopardy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jeopardy_db',
+        'USER': 'jeopardy_admin',
+        'PASSWORD': 'bobobo',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
