c5ba3c9176653ea39cb5959e27c1aee3765c2cf7
Oliver Zhang <bestozhang@gmail.com>
2016-07-17 20:23:14 -0400

    Replaced stock custom views with generic views.

--- creator/views.py
+++ creator/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'creator/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'creator/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'creator/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'creator/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'creator/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'creator/results.html'
 
 
 def vote(request, question_id):
