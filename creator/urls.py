f4f77321e4364dd4d9d4f18a992dc45711afdac5
Oliver Zhang <bestozhang@gmail.com>
2016-07-31 21:23:00 -0400

    Added very basic login functionality.

--- creator/urls.py
+++ creator/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'creator'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^login$', views.login_user, name='login_user'),
+    url(r'^logout$', views.logout_user, name='logout_user'),
     url(r'^user/(?P<pk>[0-9]+)/$', views.UserView.as_view(), name='user'),
     url(r'^game/(?P<pk>[0-9]+)/$', views.GameView.as_view(), name='game'),
 ]
--- creator/views.py
+++ creator/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import authenticate, login, logout
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.views import generic
 
 from .models import User, Game, Category, Clue, Answer
@@ -19,3 +23,29 @@ class UserView(generic.DetailView):
 class GameView(generic.DetailView):
     model = Game
     template_name = 'creator/game.html'
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('creator:user', args=(user.id,)))
+            else:
+                return render(request, 'creator/login.html', {
+                    'error_message': "Your user is deactivated. Please contact the administrator.",
+                })
+        else:
+            return render(request, 'creator/login.html', {
+                'error_message': "You have entered invalid credentials. Please try again.",
+            })
+    else:
+        return render(request, 'creator/login.html',)
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('creator:index', args=()))
